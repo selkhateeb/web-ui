c696b9d5ace9cc312ecfec733780400ebd8a61d7
Sigmund Cherem <sigmund@google.com>
2013-06-17 15:42:01 -0700

    fix rendertest: we moved smart string matcher to unittest
    Review URL: https://chromiumcodereview.appspot.com//17332002

--- lib/testing/render_test.dart
+++ lib/testing/render_test.dart
@@ -86,92 +86,9 @@ void renderTests(String baseDir, String inputDir, String expectedDir,
         var expectedPath = path.join(expectedDir, '$filename.txt');
         new File(outPath).writeAsStringSync(output);
         var expected = new File(expectedPath).readAsStringSync();
-        expect(output, new SmartStringMatcher(expected),
+        expect(output, expected,
           reason: 'unexpected output for <$filename>');
       });
-    }
-  }
-}
-
-// TODO(jmesserly): we need a full diff tool.
-// TODO(sigmund): consider moving this matcher to unittest
-class SmartStringMatcher extends BaseMatcher {
-  final String _value;
-
-  SmartStringMatcher(this._value);
-
-  bool matches(item, MatchState mismatchState) => _value == item;
-
-  Description describe(Description description) =>
-      description.addDescriptionOf(_value);
-
-  Description describeMismatch(item, Description mismatchDescription,
-      MatchState matchState, bool verbose) {
-    if (item is! String) {
-      return mismatchDescription.addDescriptionOf(item).add(' not a string');
-    } else {
-      var buff = new StringBuffer();
-      buff.write('Strings are not equal.');
-      var escapedItem = _escape(item);
-      var escapedValue = _escape(_value);
-      int minLength = min(escapedItem.length, escapedValue.length);
-      int start;
-      for (start = 0; start < minLength; start++) {
-        if (escapedValue.codeUnitAt(start) != escapedItem.codeUnitAt(start)) {
-          break;
-        }
-      }
-      if (start == minLength) {
-        if (escapedValue.length < escapedItem.length) {
-          buff.write(' Both strings start the same, but the given value also'
-              ' has the following trailing characters: ');
-          _writeTrailing(buff, escapedItem, escapedValue.length);
-        } else {
-          buff.write(' Both strings start the same, but the given value is'
-              ' missing the following trailing characters: ');
-          _writeTrailing(buff, escapedValue, escapedItem.length);
-        }
-      } else {
-        buff.write('\nExpected: ');
-        _writeLeading(buff, escapedValue, start);
-        buff.write('[32m');
-        buff.write(escapedValue[start]);
-        buff.write('[0m');
-        _writeTrailing(buff, escapedValue, start + 1);
-        buff.write('\n But was: ');
-        _writeLeading(buff, escapedItem, start);
-        buff.write('[31m');
-        buff.write(escapedItem[start]);
-        buff.write('[0m');
-        _writeTrailing(buff, escapedItem, start + 1);
-        buff.write('[32;1m');
-        buff.write('\n          ');
-        for (int i = (start > 10 ? 14 : start); i > 0; i--) buff.write(' ');
-        buff.write('^  [0m');
-      }
-
-      return mismatchDescription.replace(buff.toString());
-    }
-  }
-
-  static String _escape(String s) =>
-      s.replaceAll('\n', '\\n').replaceAll('\r', '\\r').replaceAll('\t', '\\t');
-
-  static String _writeLeading(StringBuffer buff, String s, int start) {
-    if (start > 10) {
-      buff.write('... ');
-      buff.write(s.substring(start - 10, start));
-    } else {
-      buff.write(s.substring(0, start));
-    }
-  }
-
-  static String _writeTrailing(StringBuffer buff, String s, int start) {
-    if (start + 10 > s.length) {
-      buff.write(s.substring(start));
-    } else {
-      buff.write(s.substring(start, start + 10));
-      buff.write(' ...');
     }
   }
 }
